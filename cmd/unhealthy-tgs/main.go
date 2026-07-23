ed599ff2c2d7ee56a5125fb1627044fb601bcb75
Oleksii Milevskyi <Oleksii.Milevskyi@aerlingus.com>
2026-07-24 00:04:15 +0300

    feat(cmd/unhealthy-tgs): Add multi-environment processing (but "operation error Elastic Load Balancing v2: DescribeTags, context deadline exceeded")

--- cmd/unhealthy-tgs/main.go
+++ cmd/unhealthy-tgs/main.go
@@ -17,39 +17,36 @@ import (
 )
 
 const (
-	timeoutSec = 60
+	timeoutSec = 30
 
-	// maxDescribeTagsResources = 20 // DescribeTags accepts at most 20 resource ARNs per request
-	estimatedLoadBalancerCount = 64
+	maxDescribeTagsResources = 20 // DescribeTags accepts at most 20 resource ARNs per request
 
-	eiEnvValue = "devo1"
+	eiSep = "-"
 )
 
-var eiEnvKeys = []string{
-	"ei:environment",
-	"Environment",
+var eiTags = map[string]bool{
+	"Environment":            true,
+	"ei:environment":         true,
+	"ei:provisioning-source": false,
 }
 
-func arnsWithTag(ctx context.Context, c *elasticloadbalancingv2.Client, arns []string, envName string, keys ...string) []string {
+func arnsByTags(ctx context.Context, c *elasticloadbalancingv2.Client, arns []string, tags map[string]bool, envName string) []string {
 	if len(arns) > 0 {
 		out, err := c.DescribeTags(
 			ctx, &elasticloadbalancingv2.DescribeTagsInput{ResourceArns: arns},
 		)
 		if err == nil {
 			var result []string
+			envPath := env2path(envName)
 			for _, td := range out.TagDescriptions {
-				// spew.Dump(td)
-			outer:
 				for _, tag := range td.Tags {
-					for _, key := range keys {
-						if key == *tag.Key && envName == *tag.Value || *tag.Key == "ei:provisioning-source" && strings.Contains(*tag.Value, env2path(envName)) {
-							// fmt.Println(" ", *tag.Key, *tag.Value)
+					if strict, ok := tags[*tag.Key]; ok {
+						if strict && *tag.Value == envName || !strict && strings.Contains(*tag.Value, envPath) {
 							result = append(result, *td.ResourceArn)
-							break outer
+							break
 						}
 					}
 				}
-				// fmt.Println()
 			}
 			if len(result) > 0 {
 				return result
@@ -68,37 +65,30 @@ func main() {
 	cfg, err := config.LoadDefaultConfig(ctx)
 	ut.IsErr(err, 201)
 
-	// fmt.Printf("#%v\n", cfg)
-	// fmt.Println(string(ut.Must(json.MarshalIndent(cfg, "", "  "))))
-	// spew.Dump(cfg)
-
 	var arns []string
-
-	if isatty.IsTerminal(os.Stdin.Fd()) {
+	if n := len(os.Args); isatty.IsTerminal(os.Stdin.Fd()) && n > 1 {
 		client := elasticloadbalancingv2.NewFromConfig(cfg)
 		lbs, err := describeLoadBalancers(ctx, client)
 		ut.IsErr(err, 201)
 
-		arns = make([]string, 0, len(lbs)/16) // estimatedLoadBalancerCount
-		// chunkArns := make([]string, 0, maxDescribeTagsResources)
-		// for i := 0; i < len(lbs); i++ {
-		// 	chunkArns = append(chunkArns, *lbs[i].LoadBalancerArn)
-		// 	if len(chunkArns) == cap(chunkArns) {
-		// 		// spew.Dump(chunkArns)
-		// 		// fmt.Println()
-		// 		arns = append(arns, arnsWithTag(ctx, client, chunkArns, eiEnvValue, eiEnvKeys...)...)
-		// 		chunkArns = chunkArns[:0]
-		// 	}
-		// }
-		// arns = append(arns, arnsWithTag(ctx, client, chunkArns, eiEnvValue, eiEnvKeys...)...)
-		for i := 0; i < len(lbs); i++ {
-			if strings.HasPrefix(*lbs[i].LoadBalancerName, eiEnvValue+"-") {
-				arns = append(arns, *lbs[i].LoadBalancerArn)
+		arns = make([]string, 0, (n-1)*len(lbs)/16) // estimatedLoadBalancerCount
+		chnkArns := make([]string, 0, maxDescribeTagsResources)
+
+		for j := 0; j < len(lbs); j++ {
+			for i := 1; i < n; i++ {
+				if strings.HasPrefix(*lbs[j].LoadBalancerName, os.Args[i]+eiSep) || strings.HasSuffix(*lbs[j].LoadBalancerName, eiSep+os.Args[i]) {
+					arns = append(arns, *lbs[j].LoadBalancerArn)
+				} else {
+					chnkArns = append(chnkArns, *lbs[j].LoadBalancerArn)
+					if len(chnkArns) == cap(chnkArns) {
+						arns = append(arns, arnsByTags(context.Background(), client, chnkArns, eiTags, os.Args[i])...)
+						chnkArns = chnkArns[:0]
+					}
+				}
+				arns = append(arns, arnsByTags(context.Background(), client, chnkArns, eiTags, os.Args[i])...)
+				chnkArns = chnkArns[:0]
 			}
 		}
-
-		// spew.Dump(chunkArns)
-		// fmt.Println()
 	} else {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
